de2821dcacd20f7905b5a586f3f724a48c12699a
ShreyKhanna <shrey_khanna@ymail.com>
2019-07-06 11:04:45 +0800

    Added Signup and Login Page
    ChangeLog:
    -Added Signup Page
    -Added Login Page

--- src/pages/login.js
+++ src/pages/login.js
@@ -3,11 +3,11 @@ import TextField from '@material-ui/core/TextField'
 import withStyles from '@material-ui/core/styles/withStyles'
 import PropTypes from 'prop-types'
 import axios from 'axios'
-
+import {Link} from 'react-router-dom'
 import Grid from '@material-ui/core/Grid'
 import Typography from '@material-ui/core/Typography'
 import Button from '@material-ui/core/Button'
-
+import CircularProgress from '@material-ui/core/CircularProgress'
 const styles={
 form:{
   textAlign:'center'
@@ -19,7 +19,16 @@ textField:{
   margin :'10px auto 10px auto'
 },
 button:{
-  margin:'20px'
+  margin:'20px',
+  position:'relative'
+},
+customError:{
+  color:'red',
+  fontSize:'0.8rem',
+  marginTop:'10px'
+},
+progress:{
+  position:'absolute'
 }
 }
 
@@ -40,6 +49,7 @@ class login extends React.Component{
      password:this.state.password
    }
    axios.post('/login',userData).then(result=>{
+     localStorage.setItem('FBIDToken',`Bearer ${result.data.token}`);
      console.log(result.data);
      this.setState({
        loading:false
@@ -66,12 +76,26 @@ class login extends React.Component{
       <Grid item sm>
       <Typography variant="h4" className={classes.pageTitle}>Login</Typography>
       <form noValidate onSubmit={this.handleSubmit}>
-      <TextField required id="email" name="email"  type="email" label="Email ID" className={classes.textField}
+      <TextField id="email" name="email"  type="email" label="Email ID" className={classes.textField}
       value={this.state.email} onChange={this.handleChange} helperText={errors.email} fullWidth/>
-      <TextField required id="password" name="password" label="Password" type="password" error={errors.email ? true : false}
-       className={classes.textField} value={this.state.password} onChange={this.handleChange} helperText={errors.password} error={errors.password ? true : false} 
+      <TextField id="password" name="password" label="Password" type="password" error={errors.email ? true : false}
+       className={classes.textField} value={this.state.password} onChange={this.handleChange} helperText={errors.password} error={errors.password ? true : false}
       fullWidth/>
-      <Button type="submit" variant="contained" color="primary" className={classes.button}>Submit</Button>
+      {errors.general && (
+        <Typography variant="body2" className= {classes.customError}>
+          {errors.general}
+        </Typography>
+      )}
+      <Button type="submit" variant="contained" color="primary" className={classes.button}>
+      Login
+      {
+        loading &&(
+            <CircularProgress size={20} className={classes.progress}/>
+      )
+    }
+      </Button>
+      <br/>
+      <small>Dont have an account? Sign up <Link to="/signup">here</Link></small>
       </form>
       </Grid>
       <Grid item sm>
--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,32 +1,79 @@
 import React from 'react'
 import TextField from '@material-ui/core/TextField'
+import PropTypes from 'prop-types'
+import withStyles from '@material-ui/core/styles/withStyles'
+
+import Grid from '@material-ui/core/Grid'
+import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button'
+import {Link} from 'react-router-dom'
+
+const styles={
+form:{
+    textAlign:'center'
+  },
+  pageTitle:{
+    margin:'20px auto 20px auto'
+  },
+  textField:{
+    margin :'10px auto 10px auto'
+  },
+  button:{
+    margin:'20px'
+  }
+}
 export class signup extends React.Component{
+
+  state={
+    email:"",
+    password:"",
+    confirmpassword:"",
+    handle:"",
+    errors:{}
+  }
+  handleSubmit=(event)=>{
+    event.preventDefault()
+    this.setState({
+      loading:false
+    })
+    const newuserData={
+      email:this.state.email,
+      password:this.state.password,
+      confirmpassword:this.state.confirmpassword,
+      handle:this.state.handle
+    }
+  }
+  handleChange=(event)=>{
+    this.setState({
+    [event.target.name]:event.target.value
+  });
+}
   render(){
+    const {classes}=this.props;
+    const {errors,loading}=this.state
+
     return(
-      <div className="container">
-      <form>
-      <TextField
-      required
-      id="id_email"
-      label="Email ID"
-      />
-      <TextField
-      required
-      id="id_password"
-      label="Password"
-      type="password"
-      margin="normal"
-      />
-      <TextField
-      required
-      id="id_confirmpassword"
-      label="Confirm Password"
-      type="password"
-      margin="normal"
-      />
+      <Grid container className={classes.form}>
+      <Grid item sm/>
+      <Grid item sm>
+      <Typography variant="h4" className={classes.pageTitle}>Signup</Typography>
+      <form noValidate onSubmit={this.handleSubmit}>
+      <TextField id="email"           className={classes.textField}      name="email"           label="Email ID"   value={this.state.email} fullWidth />
+      <TextField id="password"         className={classes.textField}      name="password"        label="Password" type="password" value={this.state.password} margin="normal" fullWidth/>
+      <TextField id="confirmpassword" className={classes.textField} name="confirmpassword" label="Confirm Password" value={this.state.confirmpassword} type="password" margin="normal" fullWidth/>
+      <TextField id="handle" className={classes.textField} name="handle" label="Handle" value={this.state.handle} margin="normal" fullWidth/>
+      <Button className={classes.button} type="submit" className={classes.textField} variant="contained" color="primary">SignUp</Button>
+      <br/>
+      <small>Already have an account ? Login <Link to="/login">here</Link></small>
       </form>
-      </div>
+      </Grid>
+      <Grid item sm/>
+
+      </Grid>
     )
   }
 }
-export default signup;
+signup.propTypes={
+  classes:PropTypes.object.isRequired
+}
+export default withStyles(styles)(signup);
